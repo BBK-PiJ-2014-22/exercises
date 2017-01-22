85f32fe204a6f112ee299ae0800125db50e5f7e6
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2017-01-22 19:02:28 +0000

    Report added

--- week01/Hammurabi/src/hammurabi.scala
+++ week01/Hammurabi/src/hammurabi.scala
@@ -14,11 +14,38 @@ object hammurabi extends App {
   your term. Rule poorly and you will be kicked out of office!""")
   }
   
+
   def hammurabi() = {
+    
+    var starved = 0 // how many people starved
+    var immigrants = 5 // how many people came to the city
+    var population = 100
+    var harvest = 3000 // total bushels harvested
+    var bushelsPerAcre = 3 // amount harvested for each acre planted
+    var rats_ate = 200 // bushels destroyed by rats
+    var bushelsInStorage = 2800
+    var acresOwned = 1000
+    var pricePerAcre = 19 // each acre costs this many bushels
+    var plagueDeaths = 0
+    
     printIntroductoryMessage()
+    
+    for (year <- 1 to 10) {
+          println("O great Hammurabi!")
+          println("You are in year " + year + " of your ten year rule.")
+          println("In the previous year " + starved + " people starved to death.")
+          println("In the previous year " + immigrants + " people entered the kingdom.")
+          println("The population is now " + population + ".")
+          println("We harvested " + harvest + "bushels at " + bushelsPerAcre + " bushels per acre.")
+          println("Rats destroyed " + rats_ate + " bushels, leaving " + bushelsInStorage + "bushels in storage.")
+          println("The city owns " + acresOwned + "acres of land.")
+          println("Land is currently worth " + pricePerAcre + "bushels per acre.")
+          println("There were " + plagueDeaths + " deaths from the plague.")
+    }
+               
   }
   
   hammurabi()
-  println("Check 2")
+  println("Check 3")
   
 }
