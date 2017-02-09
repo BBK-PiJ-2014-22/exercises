ee62bcf6da73e339e5aa4a9695ea3f68ed01ba41
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2017-02-09 18:28:40 +0000

    week04

--- week05_class/src/main/TrafficLight.scala
+++ week05_class/src/main/TrafficLight.scala
@@ -3,19 +3,20 @@ package main
 
 /**Represents a traffic light. Red / Amber / Green are valid**/
 sealed trait TrafficLight { 
+  def next: TrafficLight
+}
+
   
-  final case object Green extends TrafficLight //final makes it so it is impossible to extend Green etc
-  final case object Amber extends TrafficLight
-  final case object Red extends TrafficLight
-  
-  def nextPoly() = {
-        
-  }
-  
-  def nextPattern(a : TrafficLight) : TrafficLight = {
-    case Red => Green
-    case Green => Amber
-    case Amber => Red
-  }
+final case object Green extends TrafficLight {
+  def next() = Amber
+}
+
+final case object Amber extends TrafficLight {
+    def next() = Red
+
+}
+
+final case object Red extends TrafficLight{
+  def next() = Green
   
 }
