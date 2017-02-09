f624d11f85fb05d6e53a86f448f10ba2f5ec9099
BBK-PiJ-2014-22 <jmaciv02@dcs.bbk.ac.uk>
2017-02-09 18:36:54 +0000

    TrafficLight matching added

--- week05_class/src/main/TrafficLight.scala
+++ week05_class/src/main/TrafficLight.scala
@@ -2,21 +2,37 @@
 package main
 
 /**Represents a traffic light. Red / Amber / Green are valid**/
-sealed trait TrafficLight { 
-  def next: TrafficLight
+//polymorphic
+//sealed trait TrafficLight { 
+//  def next: TrafficLight
+//}
+//  
+//final case object Green extends TrafficLight {
+//  def next() = Amber
+//}
+//
+//final case object Amber extends TrafficLight {
+//    def next() = Red
+//
+//}
+//
+//final case object Red extends TrafficLight{
+//  def next() = Green
+//  
+//}
+
+
+sealed trait TrafficLight {
+  def next: TrafficLight = {
+    this match {
+      case Green => Amber
+      case Amber => Red
+      case Red => Green
+      //case _ => Anything - the _ is the wild card can can be useful at the end to catch remainder
+    }
+  }
 }
 
-  
-final case object Green extends TrafficLight {
-  def next() = Amber
-}
-
-final case object Amber extends TrafficLight {
-    def next() = Red
-
-}
-
-final case object Red extends TrafficLight{
-  def next() = Green
-  
-}
+final case object Green extends TrafficLight
+final case object Red extends TrafficLight
+final case object Amber extends TrafficLight
